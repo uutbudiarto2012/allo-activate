d550674e43895794f7c0a1a3cf70151a8f34e519
Uut Budiarto <uutbudiarto@Uuts-MacBook-Pro.local>
2022-04-28 19:47:30 +0700

    fix

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,26 +1,27 @@
-import { useState } from 'react'
-import Confirm from '../confirm/Confirm'
-import  './Footer.scss'
+import { useState } from "react";
+import Confirm from "../confirm/Confirm";
+import "./Footer.scss";
 
 const Footer = () => {
-    const [open, setOpen] = useState(false)
-    const handleModal=()=>{
-        setOpen(true)
-    }
+  const [open, setOpen] = useState(false);
+  const handleModal = () => {
+    setOpen(true);
+  };
   return (
-    <div className='footer'>
-        <span className="term">
-        By accepting your treat, you agree to the <a href="/#" target="_blank" rel="noopener noreferrer">Terms & Conditions</a>
-        </span>
-        <button onClick={handleModal}  className='btn-get-wallet'>
-            Get My Wallet
-        </button>
-        {
-            open && <Confirm onClick={()=>setOpen(false)} />
-        }
-        
+    <div className="footer">
+      <span className="term">
+        By accepting your treat, you agree to the{" "}
+        <a href="https://www.allobank.com/privacy" target="_blank" rel="noopener noreferrer">
+          Terms &{" "}
+        </a>{" "}
+        <a href="https://www.allobank.com/term-condition" target="_blank" rel="noopener noreferrer">Conditions</a>
+      </span>
+      <button onClick={handleModal} className="btn-get-wallet">
+        Get My Wallet
+      </button>
+      {open && <Confirm onClick={() => setOpen(false)} />}
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
